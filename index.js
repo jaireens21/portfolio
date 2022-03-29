f91483d55e833e26531ca33ef10f8beb1a180ebc
Jaireen Singh <jaireen.s21@gmail.com>
2022-03-29 18:07:35 -0400

    added animations

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let readMoreBtn= document.querySelector("#readMoreBtn");
 let readLessBtn= document.querySelector("#readLessBtn");
 
-let readMoreParas=document.querySelectorAll(".hidden");
+let expandableDiv=document.querySelector("#expandableDiv");
 
 readMoreBtn.addEventListener("click", function(){
-    readMoreParas.forEach(para=>para.classList.toggle("hidden"));
+    expandableDiv.classList.toggle("hidden");
     readMoreBtn.classList.add("hidden");
     readLessBtn.classList.remove("hidden");
 });
 
 readLessBtn.addEventListener("click", function(){
-    readMoreParas.forEach(para=>para.classList.toggle("hidden"));
+    expandableDiv.classList.toggle("hidden");
     readMoreBtn.classList.remove("hidden");
     readLessBtn.classList.add("hidden");
 });
