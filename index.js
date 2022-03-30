03c3fb78c85af4f0e66b71ea833fdf7c4d9e64e5
Jaireen Singh <jaireen.s21@gmail.com>
2022-03-29 21:49:51 -0400

    readMoreBtn now working

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let readMoreBtn= document.getElementById("readMoreBtn");
+let expandableDiv=document.getElementById("expandableDiv");
 
 readMoreBtn.addEventListener("click", ()=>{
-    console.log("clicked");
+    //console.log("clicked");
+    expandableDiv.classList.toggle("expandable-open");
+    readMoreBtn.textContent=readMoreBtn.textContent.includes("Read More...")? "Read Less...":"Read More...";
     
     
 });
