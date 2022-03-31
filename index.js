e9bc89e9c4a6249f4b549788f95ae009ef4a9be0
Jaireen Singh <jaireen.s21@gmail.com>
2022-03-31 10:38:37 -0400

    animated skill lists

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 let readMoreBtn= document.getElementById("readMoreBtn");
 let expandableDiv=document.getElementById("expandableDiv");
-
 readMoreBtn.addEventListener("click", ()=>{
     //console.log("clicked");
     expandableDiv.classList.toggle("expandable-open");
     readMoreBtn.textContent=readMoreBtn.textContent.includes("Read More...")? "Read Less...":"Read More...";
     
-    
 });
+
+//making the skillLists animate in
+let skillList=document.querySelectorAll(".skillList");
+
+function callbackFunc(entries, observer)
+{
+  entries.forEach(entry => {
+    //let txt = entry.target.id + " visibility: " + entry.isIntersecting;
+    //console.log(txt);
+    //output: skillSection visibility: true OR false
+    if(entry.isIntersecting){
+        skillList.forEach(list=>list.classList.add('active'));
+    }else{
+        skillList.forEach(list=>list.classList.remove('active'));
+    }
+        
+  });
+}
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+  };
+
+let observer = new IntersectionObserver(callbackFunc, options);
+
+observer.observe(document.getElementById('skillSection'));
