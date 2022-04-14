9000c2454b573bb545eb825c7f7a6d37a61f5c55
Jaireen Singh <jaireen.s21@gmail.com>
2022-04-14 12:21:48 -0400

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-let readMoreBtn= document.getElementById("readMoreBtn");
-let expandableDiv=document.getElementById("expandableDiv");
-readMoreBtn.addEventListener("click", ()=>{
-    //console.log("clicked");
-    expandableDiv.classList.toggle("expandable-open");
-    readMoreBtn.textContent=readMoreBtn.textContent.includes("Read More...")? "Read Less...":"Read More...";
+if(process.env.NODE_ENV!=='production'){
+  require('dotenv').config();
+ }
+
+const express=require('express');
+const app=express();
+
+const path=require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,'/public')));
+
+const nodemailer = require('nodemailer'); 
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    return res.render('home.ejs');
+})
+
+
+app.post("/", (req,res)=>{
+
+    const{name,email,message}=req.body;
     
-});
-
-//making the skillLists animate in
-let skillList=document.querySelectorAll(".skillList");
-
-function callbackFunc(entries, observer)
-{
-  entries.forEach(entry => {
-    //let txt = entry.target.id + " visibility: " + entry.isIntersecting;
-    //console.log(txt);
-    //output: skillSection visibility: true OR false
-    if(entry.isIntersecting){
-        skillList.forEach(list=>list.classList.add('active'));
-    }else{
-        skillList.forEach(list=>list.classList.remove('active'));
-    }
-        
+    //send email with message 
+  const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+      user:process.env.EMAIL_ADDRESS,
+      pass:process.env.EMAIL_PASSWORD
+    },
   });
-}
-
-let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.3
+  const mailOptions = {
+    to: 'jaireen.s21@gmail.com',
+    from: 'jaireen.s21@gmail.com',
+    subject: 'NEW MESSAGE: from portfolio page',
+    text: 'Hello,\n\n' +
+    `You received a message on your portfolio page. \n\n` + 
+    `From: ${name} ( ${email} ) \n` +
+    `Message: ${message}`
   };
+  transporter.sendMail(mailOptions,(err)=>{
+    if(err){
+      console.log(err);
+    }
+  })
+//   req.flash('success', 'Your message has been received. Thank you for reaching out!');
+  console.log("email sent!");
+  //error in alert
+  return alert("Thank you for reaching out!Your message has been received. I will get in touch with you shortly.");
+  
+})
 
-let observer = new IntersectionObserver(callbackFunc, options);
-
-observer.observe(document.getElementById('skillSection'));
+app.listen(3000, ()=>{
+    console.log("listening on port 3000");
+})
