c69fcaf6b88d21bc74f975c5b407d1fb4c70254a
Jaireen Singh <jaireen.s21@gmail.com>
2022-05-17 10:32:33 -0400

    photo styling

--- public/javascripts/home2.js
+++ public/javascripts/home2.js
@@ -7,36 +7,13 @@ readMoreBtn.addEventListener("click", ()=>{
     
 });
 
-//making the skillLists animate in
-// let skillList=document.querySelectorAll(".skillList");
-
-// function callbackFunc(entries, observer)
-// {
-//   entries.forEach(entry => {
-//     //let txt = entry.target.id + " visibility: " + entry.isIntersecting;
-//     //console.log(txt);
-//     //output: skillSection visibility: true OR false
-//     if(entry.isIntersecting){
-//         skillList.forEach(list=>list.classList.add('active'));
-//     }else{
-//         skillList.forEach(list=>list.classList.remove('active'));
-//     }
-        
-//   });
-// }
-
-// let options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.3
-//   };
-
-// let observer = new IntersectionObserver(callbackFunc, options);
-
-// observer.observe(document.getElementById('skillSection'));
 
 //dismissable alert
-document.getElementById("myAlertBtn").addEventListener('click', function(){
-  // console.log("clicked on X");
-  document.getElementById("myAlert").classList.add('hidden');
-});
+let myAlertBtn=document.getElementById("myAlertBtn");
+if(myAlertBtn){
+  myAlertBtn.addEventListener('click', function(){
+    // console.log("clicked on X");
+    document.getElementById("myAlert").classList.add('hidden');
+  });
+}
+
